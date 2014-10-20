4bd415055d51cd52c59be1f13932f90e51d86077
Cunqi <cx363@nyu.edu>
2014-10-19 22:31:09 -0400

    essential element

---
+++ Leetcode/src/com/cx363/entity/ListNode.java
@@ -0,0 +1,29 @@
+package com.cx363.entity;
+
+public class ListNode {
+	int val;
+	ListNode next;
+	public ListNode(int x){
+		val = x;
+		next = null;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder("[");
+		
+		ListNode pNext = null;
+		
+		pNext = this;
+		
+		while(pNext != null){
+			builder.append(pNext.val);
+			builder.append(",");
+			pNext = pNext.next;
+		}
+		builder.deleteCharAt(builder.length() - 1);
+		builder.append("]");
+		
+		return builder.toString();
+	}
+}
