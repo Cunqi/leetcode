0c6bdfca40c099de37d98198462a8fbfbd7afa79
Cunqi <cx363@nyu.edu>
2014-11-10 15:50:24 -0500

    Data Structure Entity

---
+++ Leetcode/src/com/cx363/entity/Point.java
@@ -0,0 +1,15 @@
+package com.cx363.entity;
+
+public class Point {
+	public int x;
+	public int y;
+	public Point() {
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
